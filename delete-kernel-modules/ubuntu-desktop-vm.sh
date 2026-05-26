57fa969578a338e174aa610339d58c202cdd2e8e
Chinmay1743 <52109358+Chinmay1743@users.noreply.github.com>
2026-05-26 09:59:23 +0530

    Create ubuntu-desktop-vm.sh
    Add comments and start/end instructions

---
+++ delete-kernel-modules/ubuntu-desktop-vm.sh
@@ -0,0 +1,16 @@
+# Delete unused kernel modules to save disk space, decrease boot time, make system secure
+# by reducing attack surface.
+# This file is supposed to run in a virtual machine.
+# 
+# Do not run this in a host installation. Host configuration should be checked first then
+# a separate file can be derived from this VM script to cleanup that environment.
+#
+# This file will receive update from time to time based on new modules that I find useless.
+
+
+echo "Cleanup Script for Ubuntu VM"
+
+
+
+echo "Task completed"
+
